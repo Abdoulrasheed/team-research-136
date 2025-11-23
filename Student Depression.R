d56d0f8a4e7459e1c24666a1fe859a61d852e351
Akshi-Vanaja <vg25aah@herts.ac.uk>
2025-11-23 10:26:58 +0000

    Cleaned the rows with missing values of CGPA and Depression

--- Student Depression.R
+++ Student Depression.R
@@ -9,3 +9,6 @@ print("Total observations in dataset:")
 print(nrow(data))
 print("Student observations:")
 print(nrow(data_students))
+
+# Clean data; Remove any rows with missing CGPA or Depression values
+data_clean <- data_students %>% filter(!is.na(CGPA) & !is.na(Depression))
